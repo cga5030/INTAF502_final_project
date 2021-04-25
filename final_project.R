6c1ef0fab652d58cbcf8015908094a0fa52f157f
Carl Fredrick G. Aquino <51449688+cga5030@users.noreply.github.com>
2021-04-25 10:34:03 -0400

    Populate Repo

---
+++ final_project.R
@@ -0,0 +1,159 @@
+rm(list=ls())
+graphics.off()
+
+library(ggplot2)
+library(gridExtra)
+# library(GGally)
+# library(PerformanceAnalytics)
+
+###############################################################
+# A conflict-year dataset with information on armed conflict 
+# where at least one party is the government of a state in 
+# the time period 1946-2019.
+
+load("data/ucdp-prio-acd-201.RData")
+
+# Count by year
+statedata <- ucdp_prio_acd_201
+stateyears <- statedata$year
+stateyears_unique <- sort(unique(statedata$year))
+stateconflicts <- data.frame(matrix(999,ncol=2,nrow=length(stateyears_unique)))
+stateconflicts[,1] <- stateyears_unique
+names(stateconflicts) <- c("year","count")
+
+for (n in 1:length(stateyears_unique)){
+stateconflicts[n,2] <- sum(stateyears==stateyears_unique[n])
+}
+
+# # plot 
+# plot(x=stateconflicts$year,y=stateconflicts$count,
+#      ylab = "global armed conflicts involving a state government",
+#      xlab = "year")
+
+# ggplot
+stateplot  <-  ggplot(data=stateconflicts, aes(x=year, y=count)) + 
+                geom_point() +
+                # theme(panel.grid.major = element_blank(), 
+                #       panel.grid.minor = element_blank()) +
+                ylab("Global armed conflicts (state)") + xlab("Year")#+
+                # geom_smooth()
+                # geom_smooth(aes(colour="LOESS"))+
+                # scale_colour_manual(name="legend", values=c("red"))
+print(stateplot)
+
+###############################################################
+# A conflict-year dataset containing information on communal 
+# and organized armed conflict where none of the parties is 
+# the government of a state. 
+
+load("data/ucdp-nonstate-201.RData")
+
+# Count by year
+nonstatedata <- ucdp_nonstate_201
+nonstateyears <- nonstatedata$year
+nonstateyears_unique <- sort(unique(nonstatedata$year))
+nonstateconflicts <- data.frame(matrix(999,ncol=2,nrow=length(nonstateyears_unique)))
+nonstateconflicts[,1] <- nonstateyears_unique
+names(nonstateconflicts) <- c("year","count")
+
+for (n in 1:length(nonstateyears_unique)){
+  nonstateconflicts[n,2] <- sum(nonstateyears==nonstateyears_unique[n])
+}
+
+# plot 
+# plot(x=nonstateconflicts[,1],y=nonstateconflicts[,2],
+#      ylab = "global armed conflicts involving no state government",
+#      xlab = "year")
+
+# ggplot
+nonstateplot <- ggplot(data=nonstateconflicts, aes(x=year, y=count)) + 
+                geom_point() +
+                # theme(panel.grid.major = element_blank(), 
+                #       panel.grid.minor = element_blank()) +
+                ylab("Global armed conflicts (non-state)") + xlab("Year")#+
+                # geom_smooth()
+                # geom_smooth(aes(colour="LOESS"))+
+                # scale_colour_manual(name="legend", values=c("red"))
+print(nonstateplot)
+
+##############################################################
+# Global temperature anomaly from NASA
+
+tempdata.in <- read.delim("data/temperature_anomaly.txt",sep="")
+
+temps <- data.frame(matrix(999,ncol=2,nrow=length(tempdata.in$X1880)))
+names(temps) <- c("year","temp")
+temps[,1] <- tempdata.in$X1880
+temps[,2] <- tempdata.in$X.0.16
+
+# plot
+# plot(x=temps[1,],y=temps[2,],
+#      xlab = "year",
+#      ylab = "mean temperature anomaly in deg C")
+
+anomalyplot <-  ggplot(data=temps, aes(x=year, y=temp)) + 
+                geom_point() +
+                # theme(panel.grid.major = element_blank(), 
+                #       panel.grid.minor = element_blank()) +
+                ylab("Mean global temperature anomaly [deg C]") + xlab("Year") #+
+                # geom_smooth()
+                # geom_smooth(aes(colour="LOESS"))+
+                # scale_colour_manual(name="legend", values=c("red"))
+print(anomalyplot)
+
+##############################################################
+# Correlation between temperature anomaly and state conflicts
+
+# line up the years
+begyear <- stateconflicts$year[1]
+endyear <- stateconflicts$year[length(stateconflicts$year)]
+statetemps = subset(temps, year>=begyear&year<=endyear) # trim to desired years
+
+statecor <- cor.test(x=stateconflicts$count,y=statetemps$temp
+                     ,method="spearman", exact=FALSE)
+
+stateplotdata <- stateconflicts
+stateplotdata$temps <- statetemps$temp
+stateplotdata <- subset (stateplotdata, select = -year)
+
+statecorplot <- ggplot(data=stateplotdata, aes(x=temps,y=count)) +
+                geom_point() +
+                ylab("Global armed conflict (state)") +
+                xlab("Mean Temperature Anomaly [deg C]") +
+                geom_smooth()
+                # geom_smooth(aes(colour="LOESS"))+
+                # scale_colour_manual(name="legend", values=c("red"))
+print(statecorplot)
+
+# ggpairs(stateplotdata)
+# chart.Correlation(stateplotdata)
+
+##############################################################
+# Correlation between temperature anomaly and state conflicts
+
+# line up the years
+begyear <- nonstateconflicts$year[1]
+endyear <- nonstateconflicts$year[length(nonstateconflicts$year)]
+nonstatetemps = subset(temps, year>=begyear&year<=endyear) # trim to desired years
+
+nonstatecor <- cor.test(x=nonstateconflicts$count,y=nonstatetemps$temp
+                     ,method="spearman", exact=FALSE)
+
+nonstateplotdata <- nonstateconflicts
+nonstateplotdata$temps <- nonstatetemps$temp
+nonstateplotdata <- subset (nonstateplotdata, select = -year)
+
+nonstatecorplot <- ggplot(data=nonstateplotdata, aes(x=temps,y=count)) +
+  geom_point() +
+  ylab("Global armed conflict (non-state)") + 
+  xlab("Mean Temperature Anomaly [deg C]") +
+  geom_smooth()
+  # geom_smooth(aes(colour="LOESS"))+
+  # scale_colour_manual(name="legend", values=c("red"))
+print(nonstatecorplot)
+
+##############################################################
+# grid arrange for plots
+grid.arrange(stateplot,nonstateplot,anomalyplot,ncol=3)
+
+grid.arrange(statecorplot,nonstatecorplot,ncol=2)
